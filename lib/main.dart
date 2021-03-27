168bd8fca2712b719d39c904b4dfaf203c70f0c7
yaymalaga <yaymalaga@gmail.com>
2021-03-27 14:17:14 +0100

    Add header

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: Scaffold(
+        body: Home(),
+      ),
     );
   }
 }
@@ -30,31 +32,83 @@ class MyApp extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 42,
-              horizontal: 36,
-            ),
-            color: Colors.green,
-            child: Container(
-              color: Colors.blue,
-            ),
+        Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 42,
+            horizontal: 36,
+          ),
+          color: Colors.orange,
+          child: Row(
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: Text(
+                    "YM",
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(left: 48),
+                  color: Colors.purple,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Name Namename Namename",
+                        overflow: TextOverflow.clip,
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                      Text(
+                        "Prime minister of New Zealand".toUpperCase(),
+                        overflow: TextOverflow.clip,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
         Expanded(
-          flex: 2,
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 42,
-              horizontal: 36,
-            ),
-            color: Colors.red,
-            child: Container(
-              color: Colors.yellow,
-            ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 42,
+                    horizontal: 36,
+                  ),
+                  color: Colors.green,
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 42,
+                    horizontal: 36,
+                  ),
+                  color: Colors.red,
+                  child: Container(
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ],
