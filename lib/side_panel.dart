fa4688fc18438d50521355fffac5518cc6e1da5f
yaymalaga <yaymalaga@gmail.com>
2021-03-27 21:15:48 +0100

    Some SidePanel work

--- lib/side_panel.dart
+++ lib/side_panel.dart
@@ -8,18 +8,94 @@ class SidePanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          "Name Namename Namename",
-          style: Theme.of(context).textTheme.headline1?.copyWith(height: 1),
+          "Topic 1".toUpperCase(),
+          style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                color: Colors.grey.shade900,
+                fontWeight: FontWeight.w900,
+                fontSize: 18,
+              ),
         ),
-        Text(
-          "DFKSDFK DSFKDSNF DFS",
-          style: Theme.of(context).textTheme.headline1?.copyWith(height: 1),
+        Container(
+          margin: const EdgeInsets.only(top: 6),
+          child: Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                  color: Colors.grey.shade700,
+                ),
+          ),
         ),
+        SizedBox(height: 30),
         Text(
-          "qwqqwqe ewfrewg ewfew",
-          style: Theme.of(context).textTheme.headline1?.copyWith(height: 1),
+          "Personal details".toUpperCase(),
+          style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                color: Colors.grey.shade900,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          "Birthday",
+          style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                color: Colors.lightBlue.shade700,
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 6),
+          child: Text(
+            "13 / 05 / 21",
+            style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                  color: Colors.grey.shade700,
+                ),
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          "Nationality",
+          style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                color: Colors.lightBlue.shade700,
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 6),
+          child: Text(
+            "Lorem ipsum",
+            style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                  color: Colors.grey.shade700,
+                ),
+          ),
+        ),
+        SizedBox(height: 30),
+        Text(
+          "Contact".toUpperCase(),
+          style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                color: Colors.grey.shade900,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 6),
+          child: Text(
+            "admin@anonymous.org",
+            style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                  color: Colors.grey.shade700,
+                ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 10),
+          child: Text(
+            "github.com/anonymous",
+            style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                  color: Colors.grey.shade700,
+                ),
+          ),
         ),
       ],
     );
