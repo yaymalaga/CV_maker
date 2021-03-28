80229d088a5548452b028d3e7a00a8da73e0b575
yaymalaga <yaymalaga@gmail.com>
2021-03-28 22:08:39 +0200

    Change font again!

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
-        textTheme: GoogleFonts.interTextTheme(
+        textTheme: GoogleFonts.quicksandTextTheme(
           Theme.of(context).textTheme,
         ),
       ),
