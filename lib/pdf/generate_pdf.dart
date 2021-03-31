ae995b5320311a5562fddb28f06ab1e11d00af82
yaymalaga <yaymalaga@gmail.com>
2021-03-31 21:01:05 +0200

    Set custom font in the pdf

--- lib/pdf/generate_pdf.dart
+++ lib/pdf/generate_pdf.dart
@@ -1,17 +1,22 @@
 import 'dart:typed_data';
 
+import 'package:flutter/services.dart';
 import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
+import 'package:pdf/widgets.dart';
 
 import 'layout_pdf.dart';
 
 Future<Uint8List> generatePdf() async {
-  final pdf = pw.Document();
+  final pdf = Document();
 
   pdf.addPage(
-    pw.Page(
-      margin: const pw.EdgeInsets.all(0),
+    Page(
+      margin: const EdgeInsets.all(0),
       pageFormat: PdfPageFormat.a4,
+      theme: ThemeData.withFont(
+        base: Font.ttf(await rootBundle.load('assets/Quicksand_Regular.ttf')),
+        bold: Font.ttf(await rootBundle.load('assets/Quicksand_Bold.ttf')),
+      ),
       build: (context) {
         return LayoutPDF();
       },
