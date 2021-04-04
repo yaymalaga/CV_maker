99026b846fd9e5a80ba0d27a438a2a8c4dacc9db
yaymalaga <yaymalaga@gmail.com>
2021-04-04 11:00:25 +0200

    Clean up unnused non-pdf files
    * Having to maintain two layouts isn't feasible, look into code-generation in the future

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,14 @@
-import 'dart:typed_data';
-
-import 'package:cv_maker/pdf/layout_pdf.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 
 import 'pdf/generate_pdf.dart';
-import 'pdf/side_panel/side_panel.dart';
-import 'pdf/common/stepper_tile.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,7 +24,7 @@ class MyApp extends StatelessWidget {
         body: PdfPreview(
           maxPageWidth: 700,
           build: (format) => generatePdf(),
-        ), //Home(),
+        ),
       ),
     );
   }
